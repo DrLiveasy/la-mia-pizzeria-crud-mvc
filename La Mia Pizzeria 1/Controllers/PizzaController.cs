7d534137dd4696131b9ec414cff5ab6069a24c4b
DrLiveasy <youssef.saleh.hans@gmail.com>
2023-01-18 14:39:33 +0100

    creato PizzaController

---
+++ La Mia Pizzeria 1/Controllers/PizzaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace La_Mia_Pizzeria_1.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
