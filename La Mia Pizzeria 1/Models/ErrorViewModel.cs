9d4430e76ad854cd6779485d2960a32fc3e094b9
DrLiveasy <youssef.saleh.hans@gmail.com>
2023-01-18 14:36:09 +0100

    Add project files.

---
+++ La Mia Pizzeria 1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace La_Mia_Pizzeria_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
