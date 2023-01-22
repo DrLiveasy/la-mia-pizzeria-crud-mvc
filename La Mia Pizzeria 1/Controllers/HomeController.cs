66e9c839b1bacad5dbff4567664c97ecc9863995
DrLiveasy <youssef.saleh.hans@gmail.com>
2023-01-22 19:13:33 +0100

    creato un HomeController per far ritornare la view quando si fa logout

---
+++ La Mia Pizzeria 1/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace La_Mia_Pizzeria_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
